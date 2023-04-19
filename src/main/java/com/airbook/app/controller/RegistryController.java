366535a9eff32a350838f76937396bde1d7c2d76
patrykkondrat <01150244@pw.edu.pl>
2023-04-19 21:43:26 +0200

    register and login fixed

--- src/main/java/com/airbook/app/controller/RegistryController.java
+++ src/main/java/com/airbook/app/controller/RegistryController.java
@@ -5,24 +5,36 @@ import com.airbook.app.model.User;
 import com.airbook.app.repo.UserRepo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.context.request.WebRequest;
 
 @Controller
 public class RegistryController {
-    @GetMapping("/registration")
-    public String createUser(Model model) {
+
+    private final UserRepo userRepo;
+
+    public RegistryController(UserRepo userRepo) {
+        this.userRepo = userRepo;
+    }
+
+    @GetMapping("/register")
+    public String showRegisterForm(Model model) {
         User user = new User();
         model.addAttribute("user", user);
         return "registration";
     }
 
-    @PostMapping("/save")
-    public String saveUser(@ModelAttribute User user, Model model) {
+    @PostMapping("/register")
+    public String registerUser(@ModelAttribute("user") User user, Model model, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "registration";
+        }
+        user.setRoles("ROLE_USER");
+        userRepo.save(user);
         model.addAttribute("user", user);
-        return "display_form";
+
+        return "redirect:/login";
     }
 }
--- src/main/java/com/airbook/app/service/CustomUserDetailsServiceImpl.java
+++ src/main/java/com/airbook/app/service/CustomUserDetailsServiceImpl.java
@@ -2,15 +2,10 @@ package com.airbook.app.service;
 
 import com.airbook.app.model.SecUser;
 import com.airbook.app.repo.UserRepo;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.PersistenceContext;
 
 @Service
 public class CustomUserDetailsServiceImpl implements UserDetailsService {
